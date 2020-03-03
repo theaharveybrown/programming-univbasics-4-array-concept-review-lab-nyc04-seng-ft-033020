23a18460bf2da6f6571c0f1a0d97d5f14dd23ff2
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-03 19:42:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,10 +3,9 @@ def find_element_index(array, value_to_find)
   array.length.times do |index|
     if array[index] == value_to_find
       return index
-    else 
-      return nil
     end
   end
+  nil
 end
 
 def find_max_value(array)
